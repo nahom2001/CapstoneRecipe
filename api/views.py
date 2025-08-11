65e3598a45d568544fb7a4c487d35646f3a29430
Nahom Hul <nahomhulum@gmail.com>
2025-08-11 17:28:44 +0300

    adding viewsets; list endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def api_menu(request):
+    api_urls = {
+        'List Recipes': '/api/recipe-list/',
+        'Recipe Detail': '/api/recipe-detail/{id}/',
+        'Create Recipe': '/api/recipe-create/',
+        'Update Recipe': '/api/recipe-update/{id}/',
+        'Delete Recipe': '/api/recipe-delete/{id}/',
+        'Get Recipe by Category': '/api/recipe-category/{category_name}/',
+        'Get Recipe by Ingredient': '/api/recipe-ingredient/{ingredient}/',
+        'Create Category': '/api/category-create/',
+        'Token Creation': '/api/token/',
+        'Token Refresh': '/api/token/refresh/',
+        'API List': '/api/'
+    }
+    return Response(api_urls)

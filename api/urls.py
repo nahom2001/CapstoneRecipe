cf6873b0590983275617e4f4ee30a8ed543cd014
Nahom Hul <nahomhulum@gmail.com>
2025-08-13 11:37:39 +0300

    route for create

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.api_menu, name='api-menu'),
     path("recipe-list/", views.recipe_list, name="recipe-list"),
+    path("recipe-detail/<str:pk>/", views.recipe_detail, name="task-detail"),
+    path("recipe-create", views.recipe_create, name="recipe-create"),
 ]

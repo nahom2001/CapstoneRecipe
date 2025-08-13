b01277bc376ca866550953fb82784a3f15a9ea97
Nahom Hul <nahomhulum@gmail.com>
2025-08-13 11:47:24 +0300

    routes for update and delete

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("recipe-list/", views.recipe_list, name="recipe-list"),
     path("recipe-detail/<str:pk>/", views.recipe_detail, name="task-detail"),
     path("recipe-create", views.recipe_create, name="recipe-create"),
+    path("recipe-update/<str:pk>/", views.recipe_update, name="recipe-update"),
+    path("recipe-delete/<str:pk>/", views.recipe_delete, name="recipe-delete"),
 ]

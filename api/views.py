6f09a74d9dd95d3f22bffa266d95039aafc310a7
Nahom Hul <nahomhulum@gmail.com>
2025-08-13 11:39:11 +0300

    create and detail views

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
+from rest_framework import status
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -36,3 +38,12 @@ def recipe_detail(request, pk):
     recipe = Recipe.objects.get(id=pk)
     serializer = RecipeSerializer(recipe, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def recipe_create(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

d89c4d1eac5933d7df903ac748a653d88e7fc3c0
Nahom Hul <nahomhulum@gmail.com>
2025-08-13 11:06:04 +0300

    import permission mixins

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import CASCADE
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
